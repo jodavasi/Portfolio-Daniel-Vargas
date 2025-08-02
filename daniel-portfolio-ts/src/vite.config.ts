01064cc66e4dbf54baf770cb093705edc99a26fa
jodavasi <jodavasi@hotmail.com>
2025-08-02 13:35:46 -0600

    Ready 1.1

--- daniel-portfolio-ts/src/vite.config.ts
+++ daniel-portfolio-ts/src/vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Portfolio-Daniel-Vargas',
-
-})
+  base: '/Portfolio-Daniel-Vargas/', 
+});
