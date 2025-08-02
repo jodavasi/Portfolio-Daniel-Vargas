b117fc3168a6338d4fd561c119fcb465a6dd98d4
jodavasi <jodavasi@hotmail.com>
2025-08-02 13:32:08 -0600

    d

--- daniel-portfolio-ts/src/vite.config.ts
+++ daniel-portfolio-ts/src/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Portfolio-Daniel-Vargas/', // debe coincidir exactamente con el nombre de tu repo
+  base: '/Portfolio-Daniel-Vargas',
+
 })
